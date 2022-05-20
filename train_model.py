55c7ef34436e0449eb599a526a73c8f16bb0851b
nbip <nbip@dtu.dk>
2022-05-20 21:01:34 +0200

    Update trainer

--- train_model.py
+++ train_model.py
@@ -1,17 +1,22 @@
-from models.model01 import Model01
-from models.model02 import Model02
-from models.model03 import Model03
-from models.model04 import Model04
-from models.model08 import Model08
-from models.model09 import Model09
-from models.model10 import Model10
-from models.model11 import Model11
-from models.model12 import Model12
-from models.model14 import Model14
+from models.model20 import Model20
+from models.model21 import Model21
+from models.model23 import Model23
+from models.model32 import Model32
+from models.model35 import Model35
+from models.model36 import Model36
+from models.model37 import Model37
+from models.model38 import Model38
+from models.model40 import Model40
+from models.model42 import Model42
+from models.model43 import Model43
+from models.model44 import Model44
+from models.model47 import Model47
+from models.model48 import Model48
+from models.model49 import Model49
 from trainer import train
 
 if __name__ == "__main__":
     # PYTHONPATH=. CUDA_VISIBLE_DEVICES=1 nohup python -u train_model.py > train_model.log &
-    model = Model14()
+    model = Model49()
     model.val_batch()
     train(model, n_updates=1_000_000, eval_interval=1000)
